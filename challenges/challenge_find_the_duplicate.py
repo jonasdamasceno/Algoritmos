e6495b4f82ae3134ed659db56803774930422fc5
jdamasceno <jonasmichael2021@outlook.com>
2024-03-12 02:11:57 -0300

    requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
+from collections import Counter
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) == 1:
+        return False
+
+    if not all(isinstance(num, int) for num in nums):
+        return False
+
+    count = Counter(nums)
+
+    number, frequency = count.most_common(1)[0]
+
+    if frequency > 1 and number >= 0:
+        return number
+
+    return False
