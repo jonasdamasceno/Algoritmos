34c84f0d2be598600ea3b903dc72f9d52d95cd4a
jdamasceno <jonasmichael2021@outlook.com>
2024-03-12 01:51:56 -0300

    requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "invalid")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)
+
+    assert encrypt_message("bcapac", 3) == "acb_cap"
+    assert encrypt_message("bcapac", 4) == "ca_pacb"
+    assert encrypt_message("bcapac", 9) == "capacb"
