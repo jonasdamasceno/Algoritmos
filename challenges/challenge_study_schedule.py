d524119b7e89f59582551c003895eaaf12ec4925
jdamasceno <jonasmichael2021@outlook.com>
2024-03-12 01:41:11 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_periods, target_time):
+    if not target_time or not permanence_periods:
+        return None
+
+    students = 0
+
+    for period in permanence_periods:
+        if not all(isinstance(time, int) for time in period):
+            return None
+        start_time, end_time = period
+        if start_time <= target_time <= end_time:
+            students += 1
+
+    return students
